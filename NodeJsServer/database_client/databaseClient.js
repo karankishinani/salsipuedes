942e2ea950429b1f98b07939b1c11da2b72a11b6
Carlos A. Quintero <carl03q@gmail.com>
2015-11-26 07:53:10 -0500

    fixing socket io and adding databaseClient

---
+++ NodeJsServer/database_client/databaseClient.js
@@ -0,0 +1,5 @@
+function datasetup() {
+    this.version = '0.0.1';
+}
+
+module.exports = new datasetup;
--- NodeJsServer/server.js
+++ NodeJsServer/server.js
@@ -1,3 +1,5 @@
 var io = require('socket.io')(1234);
 var shortId = require('shortid');
 var datasetup = require('./database_client/databaseClient.js');
+
+console.log("Start Version " + datasetup.version);
