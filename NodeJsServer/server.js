f4c81df0196bacf7ea699b86f3085abe4d970934
karankishinani <karan_kishinani@hotmail.com>
2015-11-28 17:59:47 -0500

    Agregar socket.on('disconnect'

--- NodeJsServer/server.js
+++ NodeJsServer/server.js
@@ -4,7 +4,42 @@ var datasetup = require('./database_client/databaseClient.js');
 
 console.log("Start Version " + datasetup.version);
 
+var clients = []; // Arreglo con la lista de clientes conectados al server
+
 io.on('connection', function (socket) {
 	var currentClient;
 	//Put your socket.on events here
+
+
+	// DISCONNECT por peticion del usuario
+	// revisar si esto podría funcionar
+	socket.on('disconnect', function (){
+
+        socket.broadcast.emit('Playerdisconnected', {
+            
+            name : currentClient.data.name,
+        });
+        socket.broadcast.emit('disconnected', currentClient);
+        
+        for (i = 0; i < clients.length; i++) {
+            
+            if (clients[i].data.name === currentClient.data.name) {
+                console.log(clients[i].data.name + " has been disconnected");
+                /* Verificar si incluir esto:
+                datasetup.UpdatePosition(currentClient.data.name, currentClient.position, function (err, dataP) {
+                }); */
+                clients.splice(i,1);
+            }
+            /* Verificar si esto es necesario:
+            else {
+                console.log(clients[i].data.name + " has in Map in");
+            }*)
+            
+
+        }
+        
+    })
+
+
 });
+
