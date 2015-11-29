3ec620edb8932a2a2cd1f485a0dd482042412369
josearana <torenadel@gmail.com>
2015-11-29 14:55:32 -0500

    inicio de login

--- NodeJsServer/server.js
+++ NodeJsServer/server.js
@@ -7,4 +7,28 @@ console.log("Start Version " + datasetup.version);
 io.on('connection', function (socket) {
 	var currentClient;
 	//Put your socket.on events here
+
+
+	//TODO: agregar usuario al socket
+
+	socket.on('login', function(data){  // parametros aqui? y es data? 
+
+		var success = datasetup.searchUser(data.name, data.password, data.character); // esta funcion retornara algo?
+
+		if (success)
+		{
+			socket.broadcast.emit('playerconnected', {name: currentClient.data.name});       // nombres reservados..?
+			socket.broadcast.emit('swapPlayer', {name: currentClient.data.name, id: currentClient.id, position: currentClient.position, color: currentClient.color}); // ^
+			//add player to socket
+
+		}
+		else
+		{
+			socket.emit('loginUnsuccess', {         //referencia "app.js"
+                        message : 'User not found'
+                    });
+		}
+
+
+	});
 });
