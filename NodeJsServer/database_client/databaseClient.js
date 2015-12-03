06ad434d3b31558f0e3b14b3e02daf0e4f833348
karankishinani <karan_kishinani@hotmail.com>
2015-12-03 16:17:17 -0500

    Agregar modulos de Equipo D (dataSetup.js, client.js, server.js)

--- NodeJsServer/database_client/databaseClient.js
+++ NodeJsServer/database_client/databaseClient.js
@@ -13,3 +13,13 @@ function datasetup() {
 }
 
 module.exports = new datasetup;
+
+// Guardar datos despues de PARTIDA TERMINADA (Equipo D)
+this.guardarPartida = function(user, partida, personaje, ganador, callback) {
+    db.query("INSERT INTO Juega ( `id_usuario`, `id_partida`, `personaje`,`ganador`) VALUES (?,?,?,?)", [user, partida, personaje, ganador], function(err, data) {
+        if (err) {
+            console.error(err);
+        }
+        callback(err, data);
+    });
+};
