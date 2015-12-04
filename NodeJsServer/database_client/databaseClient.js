788de7d53c144aec7e15302492acc8fa97241c91
josearana <josearana218@gmail.com>
2015-12-03 23:16:36 -0500

    parte b db

--- NodeJsServer/database_client/databaseClient.js
+++ NodeJsServer/database_client/databaseClient.js
@@ -47,6 +47,23 @@ function datasetup() {
             callback(err, data);
          });
     };
+
+    //Equipo B
+    this.searchUser = function (user, pass, callback) {
+
+        db.query("SELECT Username, Password FROM USUARIO WHERE Username = user and Password = pass", [user, pass], function (err, data) { 
+            if (err) { 
+                console.log('usuario o password incorrecto');
+                console.error(err); 
+            }
+            else{ 
+                console.log('login exitoso');
+            }
+            callback(err, data);  //retorna algo?
+        });
+
+    };
+    
     // Guardar datos despues de PARTIDA TERMINADA (Equipo D)
     this.guardarPartida = function(user, partida, personaje, ganador, callback) {
         db.query("INSERT INTO Juega ( `id_usuario`, `id_partida`, `personaje`,`ganador`) VALUES (?,?,?,?)", [user, partida, personaje, ganador], function(err, data) {
