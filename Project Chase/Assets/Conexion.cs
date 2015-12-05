202b45bccab5d45d9065647e7bc049ac008c5efe
Carlos A. Quintero <carl03q@gmail.com>
2015-12-05 10:19:44 -0500

    Project Chase - Juego en Unity

---
+++ Project Chase/Assets/Conexion.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+/*using System.Data; 
+using System.Data.Sql; 
+using System.Data.SqlClient; 
+
+*/
+
+public class Conexion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		string connectionString =
+			"Server=server10.000webhost.com;" +
+				"Database=a7395980_dbchase;" +
+				"User ID=a7395980_dbchase;" +
+				"Password=carademierda1;";
+		
+		MySqlConnection dbcon;
+		dbcon = new MySqlConnection(connectionString);
+		dbcon.Open();
+
+
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
