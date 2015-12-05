09df695569180545c3bcddb27448b3b8f98e7e5e
Carlos A. Quintero <carl03q@gmail.com>
2015-12-05 10:22:37 -0500

    Assets

---
+++ Project Chase/Assets/SeguirJugador.js
@@ -0,0 +1,12 @@
+﻿var jugador : GameObject;
+var distancia : int = 15;
+
+function Start () {
+
+}
+
+function Update () {
+
+transform.position = Vector3(jugador.transform.position.x, jugador.transform.position.y + distancia, jugador.transform.position.z);
+
+}
